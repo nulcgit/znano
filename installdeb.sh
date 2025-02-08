2e6260202a265fde2047f7114494f685c3c17218
Nulc <nulc@ya.ru>
2025-02-09 01:07:11 +0800

    Add main.js

---
+++ installdeb.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt install -y ca-certificates curl gnupg
+sudo rm /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=22
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install nodejs -y
+sudo npm install -g npm@11.1.0
+sudo npm install -g yarn
+node -v
+npm -v
+yarn -v
